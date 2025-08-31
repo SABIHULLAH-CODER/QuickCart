7391925464362e1c2efb3bd6c72edc7250392a03
sabihulla <sabihullahjan912@gmail.com>
2025-08-31 06:46:52 -0700

    First Comment

--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -6,7 +6,6 @@ import { useAppContext } from '@/context/AppContext'
 const Navbar = () => {
 
   const { router } = useAppContext()
-
   return (
     <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
       <Image onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer' src={assets.logo} alt="" />
